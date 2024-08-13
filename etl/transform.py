6f2b6fbcb55346cd38dbc671d8bacce791ab031b
Manas <nmmanas@gmail.com>
2024-08-13 22:50:32 +0530

    transform: passing back the input without any transformation

--- etl/transform.py
+++ etl/transform.py
@@ -1,2 +1,12 @@
 def transform_data(data):
-    raise NotImplementedError
+    """
+    The `transform_data` function in Python simply returns the input data without any
+    transformation.
+
+    :param data: The `transform_data` function you provided simply returns the input
+    data as it is without any transformation.
+    :return: the `transformed_data`, which is a variable holding the same value as the
+    input `data`.
+    """
+    transformed_data = data
+    return transformed_data
