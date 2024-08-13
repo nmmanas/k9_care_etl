b9c51e8252722da2cc4dd98c65f078961fd36632
Manas <nmmanas@gmail.com>
2024-08-13 22:20:05 +0530

    extract: implemented get_resource and extract_data functions

--- etl/extract.py
+++ etl/extract.py
@@ -1,6 +1,25 @@
+import requests
+
+from .config import Config
+
+
 def get_resource():
-    raise NotImplementedError
+    """
+    The function `get_resource` retrieves a resource from a specified URL with a timeout
+    of 5 seconds.
+    :return: The function `get_resource()` is returning the response object obtained by
+    making a GET request to the resource URL specified in the `Config` object.
+    """
+    url = Config.resource_url
+    response = requests.get(url, timeout=5)
+    return response
 
 
 def extract_data():
-    raise NotImplementedError
+    """
+    The function `extract_data` retrieves a resource and returns its JSON data.
+    :return: The function `extract_data()` is returning the JSON data obtained from the
+    `get_resource()` function.
+    """
+    data = get_resource()
+    return data.json()
