eb6991f15f1ec69f20e3cd3a9fcd4ceedf7078ee
Manas <nmmanas@gmail.com>
2024-08-13 23:25:06 +0530

    load: implemented the basic function

--- etl/load.py
+++ etl/load.py
@@ -1,2 +1,32 @@
+import os
+
+import psycopg2
+
+
 def load_data(data):
-    raise NotImplementedError
+    """
+    The `load_data` function inserts data into a PostgreSQL database table named
+    `facts`.
+
+    :param data: data to be loaded
+    eg:
+    [
+        {"fact": "Dogs are loyal.", "created_date": "2024-05-12T08:17:49.657Z"},
+        {
+            "fact": "Dogs have a sense of time.",
+            "created_date": "2024-07-11T22:49:15.819Z",
+        },
+    ]
+    """
+    DB_URI = os.getenv("DB_URI")
+    conn = psycopg2.connect(DB_URI)
+    cursor = conn.cursor()
+
+    insert_query = "INSERT INTO facts (fact, created_date) VALUES (%s, %s)"
+
+    for item in data:
+        cursor.execute(insert_query, (item["fact"], item["created_date"]))
+
+    conn.commit()
+    cursor.close()
+    conn.close()
