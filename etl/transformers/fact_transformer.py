23044697fef6372715496e51e93b3a384d9c1f80
Manas <nmmanas@gmail.com>
2024-08-14 21:22:08 +0530

    fact transformer: implemented typo correction using pyspellchecker library

--- etl/transformers/fact_transformer.py
+++ etl/transformers/fact_transformer.py
@@ -1,3 +1,5 @@
+from spellchecker import SpellChecker
+
 from .base_transformer import BaseTransformer
 
 
@@ -18,4 +20,38 @@ class FactTransformer(BaseTransformer):
     def clean_whitespaces(self, data):
         for fact in data:
             fact["fact"] = " ".join(fact["fact"].split()).strip()
+        return data
+
+    def correct_typos(self, data):
+        """
+        The function corrects typos in a list of facts by using a SpellChecker to
+        correct words and maintain capitalization.
+
+        :param data: facts dictionary in following format:
+            [
+                {
+                    "fact": "fact 1",
+                    "created_date": "<date>"
+                }
+            ]
+        :return: The `correct_typos` method returns the `data` after correcting any
+        typos in the "fact" field of each item in the data. The typos are corrected
+        using the `SpellChecker` class, and the corrected words are stored back in the
+        "fact" field before returning the updated `data`.
+        """
+        spell = SpellChecker()
+
+        for fact in data:
+            corrected_words = []
+            for word in fact["fact"].split():
+                # Check if the word is capitalized or in uppercase
+                if word.isupper():
+                    corrected_word = spell.correction(word).upper()
+                elif word[0].isupper():
+                    corrected_word = spell.correction(word).capitalize()
+                else:
+                    corrected_word = spell.correction(word)
+                corrected_words.append(corrected_word)
+
+            fact["fact"] = " ".join(corrected_words)
         return data
