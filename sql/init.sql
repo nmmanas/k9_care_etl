69871bfa57e7cb973449064df8a29fa6ad2f99ea
Manas <nmmanas@gmail.com>
2024-08-15 15:42:31 +0530

    sql: init: changed schema to support versioning

--- sql/init.sql
+++ sql/init.sql
@@ -1,5 +1,12 @@
 CREATE TABLE IF NOT EXISTS facts(
-    fact_hash      CHAR(32) PRIMARY KEY,
-    fact            TEXT,
-    created_date    TIMESTAMP
+    id                      SERIAL PRIMARY KEY,
+    fact_number             INTEGER,
+    fact_hash               CHAR(32) UNIQUE,
+    fact                    TEXT,
+    created_date            TIMESTAMPTZ,
+    effective_start_date    TIMESTAMPTZ NOT NULL DEFAULT now(),
+    effective_end_date      TIMESTAMPTZ,
+    is_current              BOOLEAN NOT NULL DEFAULT TRUE
 );
+
+CREATE INDEX idx__facts__is_current ON facts (is_current);
