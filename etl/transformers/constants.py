37266461c0ce483f6dd0cd53520a06eb4c65c2a9
Manas <nmmanas@gmail.com>
2024-08-16 03:47:56 +0530

    constants: for checking is_numeric

---
+++ etl/transformers/constants.py
@@ -0,0 +1,38 @@
+# List of number words (can be expanded based on your requirements)
+number_words = {
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+    "twenty",
+    "thirty",
+    "forty",
+    "fifty",
+    "sixty",
+    "seventy",
+    "eighty",
+    "ninety",
+    "hundred",
+    "thousand",
+    "million",
+    "billion",
+}
+
+# Regular expression for matching hyphenated number words
+hyphenated_numbers_pattern = r"\b(?:twenty|thirty|forty|fifty|sixty|seventy|eighty|ninety)-(?:one|two|three|four|five|six|seven|eight|nine)\b"  # noqa
