c39330265b51228e4027b7bcd0216578cb10ea42
Manas <nmmanas@gmail.com>
2024-08-16 03:48:26 +0530

    sql init: schema changes for is_numeric

--- sql/init.sql
+++ sql/init.sql
@@ -1,15 +1,17 @@
 CREATE TABLE IF NOT EXISTS facts(
     id                      SERIAL PRIMARY KEY,
-    fact_number             INTEGER,
+    fact_number             INTEGER NOT NULL,
     fact_hash               CHAR(32) UNIQUE,
-    fact                    TEXT,
-    created_date            TIMESTAMPTZ,
+    fact                    TEXT NOT NULL,
+    created_date            TIMESTAMPTZ NOT NULL,
+    is_numeric              BOOLEAN NOT NULL DEFAULT FALSE,
     effective_start_date    TIMESTAMPTZ NOT NULL DEFAULT now(),
     effective_end_date      TIMESTAMPTZ,
     is_current              BOOLEAN NOT NULL DEFAULT TRUE
 );
 
 CREATE INDEX idx__facts__is_current ON facts (is_current);
+CREATE INDEX idx__facts__is_numeric ON facts (is_numeric);
 
 CREATE TABLE IF NOT EXISTS lsh_buckets (
     fact_id     INTEGER,
