adcb7a559da53bbd358478caa3cab4be2f429f85
Manas <nmmanas@gmail.com>
2024-08-15 23:28:18 +0530

    etl: changed main orchestrator with the changes

--- main_etl.py
+++ main_etl.py
@@ -1,23 +1,26 @@
 from etl.config import Config
 from etl.extractors import JSONURLExtractor
-from etl.loaders.postgres_loader import PostgresLoader
+from etl.loaders.facts_loader import FactsLoader
+from etl.repositories.postgres_repository import PostgresRepository
 from etl.transformers.fact_transformer import FactTransformer
 
 
 def run_etl():
     url = Config.resource_url
-    db_uri = Config.db_uri
     # Step 1: Extract the data
     extractor = JSONURLExtractor(url)
     raw_data = extractor.extract()
 
+    db_uri = Config.db_uri
+    repository = PostgresRepository(db_uri)
+
     # Step 2: Transform the data
-    transformer = FactTransformer()
-    transformed_data = transformer.transform(raw_data)
+    transformer = FactTransformer(data_repository=repository)
+    transformed_data, expired_data = transformer.transform(raw_data)
 
     # Step 3: Load the data into PostgreSQL
-    loader = PostgresLoader(db_uri)
-    loader.load(transformed_data)
+    loader = FactsLoader(data_repository=repository)
+    loader.load(transformed_data, expired_data)
 
 
 if __name__ == "__main__":
