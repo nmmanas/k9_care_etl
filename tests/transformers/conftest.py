e4efbabec37f6c5526445857daea30002fa386cd
Manas <nmmanas@gmail.com>
2024-08-15 18:21:53 +0530

    test: transform: version manager, added conftest

--- tests/transformers/conftest.py
+++ tests/transformers/conftest.py
@@ -11,3 +11,10 @@ def fact_transformer_instance(data_repository_mock):
     from ...etl.transformers import FactTransformer
 
     return FactTransformer(data_repository=data_repository_mock)
+
+
+@pytest.fixture
+def fact_version_manager(data_repository_mock):
+    from ...etl.transformers import FactVersionManager
+
+    return FactVersionManager(data_repository_mock)
