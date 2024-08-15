a1fe0c584848dde41e21ccbef79120691d00f784
Manas <nmmanas@gmail.com>
2024-08-15 15:42:57 +0530

    sql: init: added schema to store lsh_bucket values

--- sql/init.sql
+++ sql/init.sql
@@ -10,3 +10,11 @@ CREATE TABLE IF NOT EXISTS facts(
 );
 
 CREATE INDEX idx__facts__is_current ON facts (is_current);
+
+CREATE TABLE IF NOT EXISTS lsh_buckets (
+    fact_id     INTEGER,
+    bucket_hash INTEGER,
+    FOREIGN KEY(fact_id) REFERENCES facts(id)
+);
+
+CREATE INDEX idx__lsh_buckets__bucket_hash ON lsh_buckets (bucket_hash);
